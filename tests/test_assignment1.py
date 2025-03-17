c893d82204b82128c68f21648d71e2346a339bcb
Ubuntu <ubuntu@ip-172-31-36-30.ap-southeast-1.compute.internal>
2025-03-17 19:29:06 +0000

    submit-1742239746319-student-test-1

--- tests/test_assignment1.py
+++ tests/test_assignment1.py
@@ -1,6 +1,6 @@
 import pytest
 from assignments.assignment1 import add
-
+# this is test
 def test_add():
     # Verify that the add function works as expected.
     assert add(2, 3) == 5, "Expected add(2, 3) to return 5"
